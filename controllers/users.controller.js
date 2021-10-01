38ed1f1bc2ff65315e97a924cff60f95c4ce5786
jacopobacci <bacci.jaco@gmail.com>
2021-10-01 12:04:23 +0200

    get me error handling

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -6,6 +6,7 @@ exports.me = async (req, res) => {
   const user = await User.findById(req.user.userId).select("-password -isAdmin");
 
   const customer = await Customer.findOne({ user: req.user.userId }).select("-user").populate("favouriteGenres");
+  if (!customer) return res.status(404).json({ error: "You have to create a customer to see your profile" });
 
   res.status(200).json({ user, customer });
 };
